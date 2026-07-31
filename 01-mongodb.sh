26fc2ef13065a21ae4b1f8519ab3299f810189d7
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-07-31 15:40:52 +0530

    shell roboshop v2

--- 01-mongodb.sh
+++ 01-mongodb.sh
@@ -2,7 +2,7 @@
 app_name="mongodb"
 source ./common.sh
 
-# check_root
+check_root
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Adding Mongo repo"
--- common.sh
+++ common.sh
@@ -12,17 +12,19 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+SCRIPT_DIR=$PWD
+echo -e "$TIMESTAMP [INFO] Script started: $SECONDS"
 
-if [ $USERID -ne 0 ]; then
-    echo -e "$R Please run with root access $N" | tee -a $LOG_FILE
-    exit 1
-fi
-# check_root(){
-#     if [ $USERID -ne 0 ]; then
-#         echo -e "$R Please run with root access $N" | tee -a $LOG_FILE
-#         exit 1
-#     fi
-# }
+# if [ $USERID -ne 0 ]; then
+#     echo -e "$R Please run with root access $N" | tee -a $LOG_FILE
+#     exit 1
+# fi
+check_root(){
+    if [ $USERID -ne 0 ]; then
+        echo -e "$R Please run with root access $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
 function VALIDATE(){
     if [ $1 -ne 0 ]; then
         echo -e "$TIMESTAMP [ERROR] $2...  $R FAILED $N" | tee -a $LOG_FILE
@@ -30,4 +32,9 @@ function VALIDATE(){
     else 
         echo -e "$TIMESTAMP [INFO] $2... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
+}
+
+print_exec_time(){
+    TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+    echo -e "$TIMESTAMP [INFO] Script executed in $G $SECONDS seconds $N"
 }
