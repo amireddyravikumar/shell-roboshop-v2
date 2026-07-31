efb4128ce3566ac0bda19af0a218456206de202e
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-07-31 17:33:12 +0530

    shell roboshop-v2

--- 08-shipping.sh
+++ 08-shipping.sh
@@ -14,9 +14,9 @@ VALIDATE $? "created MySql Client"
 
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e "use cities" &>>$LOG_FILE
 if [ $? -ne 0 ]; then
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql 
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
     VALIDATE $? "Required Data loaded"
 else 
     echo -e "Data already loaded.. $Y SKIPPING $N"
